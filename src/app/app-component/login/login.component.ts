3d036a5777b3a16030d6f4e28008ec2a41208b7e
daredkar <redkar.darshan11@gmail.com>
2017-11-24 18:33:32 +0530

    short changes
    sc

--- src/app/app-component/login/login.component.ts
+++ src/app/app-component/login/login.component.ts
@@ -1,25 +1,40 @@
+import { HttpModule, Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [FormBuilder]
 })
 export class LoginComponent implements OnInit {
 
   open: Boolean = true;
-  constructor(private rout: Router) { }
+  username: string = '';
+  password: String = '';
+  loginForm: FormGroup;
+  constructor(private rout: Router, private fb: FormBuilder) {
+    this.loginForm = fb.group({
+      'username': [null, Validators.required],
+      'password': [null, Validators.compose([Validators.minLength(10), Validators.maxLength(50),  Validators.required])]
+    });
+  }
 
   ngOnInit() {
   }
 
-  login() {
+  login(form) {
     this.open = false;
-    localStorage.setItem('loggedIn', 'true');
-    let some = localStorage.getItem('loggedIn');
-    this.rout.navigate(['home']);
-    console.log(some);
+    if (form.username === 'darshan' && form.password === 'darshan') {
+      localStorage.setItem('loggedIn', 'true');
+      let some = localStorage.getItem('loggedIn');
+      this.rout.navigate(['home']);
+      console.log(some);
+     } else {
+       this.rout.navigate(['about']);
+     }
   }
 
   home() {
